a7a4adfdc3557abf07651a266d81f2f02a0bddd7
Daniel Totten <TottenDC@gmail.com>
2018-11-06 11:25:10 -0500

    Creates the 12 contacts on the page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
+let contactHTML = '';
 fetch('https://randomuser.me/api/?results=12&nat=us,nz,gb,au')
   .then(response => response.json())
   .then(data => {
-    console.log(data)
+    console.log(data);
+    $.each(data.results, function(index, person) {
+      let contactHTMLCard = `
+        <div class="card">
+            <div class="card-img-container">
+                <img class="card-img" src="${person.picture.large}" alt="profile picture">
+            </div>
+            <div class="card-info-container">
+                <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
+                <p class="card-text">${person.email}</p>
+                <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
+            </div>
+        </div>`;
+      contactHTML += contactHTMLCard;
+    }); // end $.each
+    $('#gallery').html(contactHTML);
   });
